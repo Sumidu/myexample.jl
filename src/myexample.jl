478c8c344480c386561f1e4d77905ee5a0995321
André Calero Valdez <andrecalerovaldez@gmail.com>
2019-10-07 12:56:26 +0200

    added demo function f plus three test cases.

--- src/myexample.jl
+++ src/myexample.jl
@@ -2,4 +2,6 @@ module myexample
 
 greet() = print("Hello World!")
 
+include("extra_file.jl")
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using myexample
 using Test
 
+myexample.f(2)
+
 @testset "myexample.jl" begin
     # Write your own tests here.
+    @test myexample.f(2) == 4
+    @test myexample.f(0) == 0
+    @test myexample.f(1) == 2
 end
